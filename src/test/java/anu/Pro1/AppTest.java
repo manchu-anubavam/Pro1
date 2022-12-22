0eeb4ba5be8451e087c781779d21ef9d83f44a6e
Anubavam <Anubavam@ANU-DES33>
2022-12-22 17:37:34 +0530

    First commit - commiting the project to github

---
+++ src/test/java/anu/Pro1/AppTest.java
@@ -0,0 +1,20 @@
+package anu.Pro1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/anu/Pro1/pro1.java
@@ -0,0 +1,11 @@
+package anu.Pro1;
+
+public class pro1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Pro1");
+		
+	}
+	
+}
