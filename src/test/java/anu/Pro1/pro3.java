6673e07bbd2158fe842f6a40760a7e4e4218451b
Anubavam <Anubavam@ANU-DES33>
2022-12-23 09:53:59 +0530

    Changes are completed, verify it and notify me

--- src/test/java/anu/Pro1/pro3.java
+++ src/test/java/anu/Pro1/pro3.java
@@ -7,7 +7,8 @@ public class pro3 {
 		System.out.println("Pro11");
 		System.out.println("Pro12");
 		System.out.println("Pro13");
-		
+		System.out.println("Pro14");
+		System.out.println("Pro15");
 	}
 	
 }
