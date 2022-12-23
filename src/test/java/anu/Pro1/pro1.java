78c7e0fc39484c54d3b090b7b7595d1182884917
Anubavam <Anubavam@ANU-DES33>
2022-12-23 10:18:34 +0530

    Done

--- src/test/java/anu/Pro1/pro1.java
+++ src/test/java/anu/Pro1/pro1.java
@@ -9,7 +9,7 @@ public class pro1 {
 		System.out.println("Pro3");
 		System.out.println("Pro9");
 		System.out.println("Pro10");
-    System.out.println("Pro16");
+		System.out.println("Pro16");
 		System.out.println("Pro17");
 	}
 	
