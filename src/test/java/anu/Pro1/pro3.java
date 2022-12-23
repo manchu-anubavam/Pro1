db79f0017129c8a595cdf061c53f192d70a0e3b3
Anubavam <Anubavam@ANU-DES33>
2022-12-23 09:48:40 +0530

    Committing to Branch2

---
+++ src/test/java/anu/Pro1/pro3.java
@@ -0,0 +1,13 @@
+package anu.Pro1;
+
+public class pro3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Pro11");
+		System.out.println("Pro12");
+		System.out.println("Pro13");
+		
+	}
+	
+}
